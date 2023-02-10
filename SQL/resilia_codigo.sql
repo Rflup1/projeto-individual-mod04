25c07b88e9f1d02cc7ef289849fae30e95052b7f
daniloxd2022 <113191711+daniloxd2022@users.noreply.github.com>
2023-02-10 11:44:23 -0300

    update

--- SQL/resilia_codigo.sql
+++ SQL/resilia_codigo.sql
@@ -13,7 +13,6 @@ contato VARCHAR(22),
 cep VARCHAR(8),
 email VARCHAR(100),
 nascimento DATE,
-cursos_id BIGINT,
 PRIMARY KEY(id)
 );
 
@@ -25,8 +24,7 @@ sexo CHAR(1),
 contato VARCHAR(22),
 cep VARCHAR(8),
 email VARCHAR(100),
-turmas_id BIGINT,
-idade INT NOT NULL,
+nascimento DATE,
 PRIMARY KEY(id)
 );
 
@@ -35,17 +33,11 @@ id BIGINT NOT NULL AUTO_INCREMENT,
 nome VARCHAR(100),
 turno VARCHAR(6),
 numero_de_alunos INT,
-aluno_id BIGINT NOT NULL,
-professor_id BIGINT NOT NULL,
-PRIMARY KEY(id)
-);
-
-CREATE TABLE unidades(
-id BIGINT NOT NULL AUTO_INCREMENT,
-nome VARCHAR(100),
-cep VARCHAR(8),
-cursos_id BIGINT,
-PRIMARY KEY(id)
+aluno_id BIGINT,
+professor_id BIGINT,
+PRIMARY KEY(id),
+FOREIGN KEY(aluno_id) REFERENCES alunos(id),
+FOREIGN KEY(professor_id) REFERENCES professores(id)
 );
 
 CREATE TABLE cursos(
@@ -55,27 +47,15 @@ descricao TEXT,
 data_comeco DATE,
 data_termino DATE,
 turmas_id BIGINT,
-unidades_id BIGINT,
-PRIMARY KEY(id)
+PRIMARY KEY(id),
+FOREIGN KEY(turmas_id) REFERENCES turmas(id)
 );
 
-ALTER TABLE turmas 
-ADD CONSTRAINT fk_alunoT FOREIGN KEY(aluno_id) REFERENCES alunos(id);
-
-ALTER TABLE turmas 
-ADD CONSTRAINT fk_professorT FOREIGN KEY(professor_id) REFERENCES professores(id);
-
-ALTER TABLE unidades
-ADD CONSTRAINT fk_cursosU FOREIGN KEY(cursos_id) REFERENCES cursos(id);
-
-ALTER TABLE cursos
-ADD CONSTRAINT fk_turmasC FOREIGN KEY(turmas_id) REFERENCES turmas(id);
-
-ALTER TABLE cursos
-ADD CONSTRAINT fk_unidadesC FOREIGN KEY(unidades_id) REFERENCES unidades(id);
-
-ALTER TABLE alunos
-ADD CONSTRAINT fk_cursosA FOREIGN KEY(cursos_id) REFERENCES cursos(id);
-
-ALTER TABLE professores
-ADD CONSTRAINT fk_turmasP FOREIGN KEY(turmas_id) REFERENCES turmas(id);
+CREATE TABLE unidades(
+id BIGINT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(100),
+cep VARCHAR(8),
+cursos_id BIGINT,
+PRIMARY KEY(id),
+FOREIGN KEY(cursos_id) REFERENCES cursos(id)
+);
